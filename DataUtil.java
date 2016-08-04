9f81d06c69e7687eec4b58356c01bd2199e9f8e7
郭晋宇 <gjyvip@yeah.net>
2016-08-04 10:55:01 +0800

    add file

---
+++ DataUtil.java
@@ -0,0 +1,76 @@
+package com.acker.android;
+
+/**
+ * Created by guojinyu on 2016-8-4.
+ */
+public class DataUtil {
+
+    /**
+     * String转Byte数组
+     *
+     * @param str
+     * @return
+     */
+    public static byte[] strToByteArray(String str) {
+        if (str == null) {
+            return null;
+        }
+        byte[] byteArray = str.getBytes();
+        return byteArray;
+    }
+
+    /**
+     * Byte数组转String
+     *
+     * @param byteArray
+     * @return
+     */
+    public static String byteArrayToStr(byte[] byteArray) {
+        if (byteArray == null) {
+            return null;
+        }
+        String str = new String(byteArray);
+        return str;
+    }
+
+    /**
+     * Byte数组转十六进制String
+     *
+     * @param byteArray
+     * @return
+     */
+    public static String byteArrayToHexStr(byte[] byteArray) {
+        if (byteArray == null) {
+            return null;
+        }
+        char[] hexArray = "0123456789ABCDEF".toCharArray();
+        char[] hexChars = new char[byteArray.length * 2];
+        for (int j = 0; j < byteArray.length; j++) {
+            int v = byteArray[j] & 0xFF;
+            hexChars[j * 2] = hexArray[v >>> 4];
+            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+        }
+        return new String(hexChars);
+    }
+
+    /**
+     * 十六进制String转Byte数组
+     *
+     * @param str
+     * @return
+     */
+    public static byte[] hexStrToByteArray(String str) {
+        if (str == null) {
+            return null;
+        }
+        if (str.length() == 0) {
+            return new byte[0];
+        }
+        byte[] byteArray = new byte[str.length() / 2];
+        for (int i = 0; i < byteArray.length; i++) {
+            String subStr = str.substring(2 * i, 2 * i + 2);
+            byteArray[i] = ((byte) Integer.parseInt(subStr, 16));
+        }
+        return byteArray;
+    }
+}
