580309763f1dd6ac212aed43c2bfe66200821cc0
Acker <acker@A-MBP.local>
2017-02-28 13:31:31 +0800

    add md5

--- DataUtil.java
+++ DataUtil.java
@@ -73,4 +73,28 @@ public class DataUtil {
         }
         return byteArray;
     }
+
+    /**
+     *  字符串md5编码
+     * 
+     * @param string
+     * @return
+     */
+    public static String md5(String string) {
+        byte[] hash;
+        try {
+            hash = MessageDigest.getInstance("MD5").digest(
+                    string.getBytes("UTF-8"));
+            StringBuilder hex = new StringBuilder(hash.length * 2);
+            for (byte b : hash) {
+                if ((b & 0xFF) < 0x10)
+                    hex.append("0");
+                hex.append(Integer.toHexString(b & 0xFF));
+            }
+            return hex.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return string;
+        }
+    }
 }
